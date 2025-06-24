02f9d6d00a1eb40f46e6b9da6b0743740e86c34f
Fernando Cantero <you@example.com>
2025-06-23 22:32:05 -0300

    arreglo de swager

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT ?? 3001;
 const { userRoute, commentRoute, post_imageRoute, postRoute, tagRoute } = require("./routes");
 const app = express()
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/', 'swagger.yaml'));
+
 
 app.use(express.json())
-//app.use(logRequest); nose que es esto
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/user", userRoute);
 app.use("/comment", commentRoute);
 app.use("/post_image", post_imageRoute);
@@ -25,4 +31,5 @@ app.listen(PORT, async (err) => {
         process.exit(1)
     }
     console.log(`App iniciada http://0.0.0.0:${PORT}`)
+    console.log(`Documentación Swagger en http://localhost:${PORT}/api-docs`);
 });
